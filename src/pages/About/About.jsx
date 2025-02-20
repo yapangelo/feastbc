1a79b5e864eeee911db64a0f15d18f284684e18c
Angelo <yapangeloterezo@gmail.com>
2025-02-20 03:21:32 -0800

    built grandfeast page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,17 @@
 import "./About.scss";
+import feastlogo from "../../assets/images/bcfeast-vertical-black.png";
+import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div className="about">
-      <h1>about</h1>
+      <div className="about__content">
+        <img src={feastlogo} alt="feast-logo" />
+        <Link to="/">
+          <Button className="button--primary" text="come home" />
+        </Link>
+      </div>
     </div>
   );
 };
