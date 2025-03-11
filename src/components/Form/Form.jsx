b923a8ed6ede5f14456a27c9475e2fc6d37f6557
Angelo <yapangeloterezo@gmail.com>
2025-03-11 02:43:32 -0700

    added consent waiver

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,6 +2,7 @@ import "./Form.scss";
 import React, { useRef, useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 import Button from "../../components/Button/Button";
+import ConsentPopup from "../ConsentPopup/ConsentPopup";
 
 const Form = ({
   title = "RESERVE YOUR TICKETS!", // Default title
@@ -17,6 +18,8 @@ const Form = ({
   const [showPopup, setShowPopup] = useState(false);
   const [popupMessage, setPopupMessage] = useState("");
   const [popupType, setPopupType] = useState("");
+  const [consentGiven, setConsentGiven] = useState(false);
+  const [showConsentPopup, setShowConsentPopup] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -69,11 +72,34 @@ const Form = ({
             required={input.required}
           />
         ))}
+        <div className="form__checkbox">
+          <input
+            type="checkbox"
+            id="consent"
+            checked={consentGiven}
+            onChange={() => setConsentGiven(!consentGiven)}
+            required
+          />
+          <label htmlFor="consent">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault(); // Prevents navigation
+                setShowConsentPopup(true); // Opens popup
+              }}
+            >
+              I agree to the Privacy Policy and consent to my information being
+              collected and processed.
+            </a>
+          </label>
+        </div>
+
         <div className="form__button-container">
           <Button
             type="submit"
             text={buttonText}
             className="button--primary form__button"
+            disabled={!consentGiven}
           />
         </div>
       </form>
@@ -82,6 +108,11 @@ const Form = ({
         <div className={`form__popup ${popupType}`}>
           <p>{popupMessage}</p>
         </div>
+      )}
+
+      {/* Show Privacy Policy Popup if triggered */}
+      {showConsentPopup && (
+        <ConsentPopup onClose={() => setShowConsentPopup(false)} />
       )}
     </section>
   );
