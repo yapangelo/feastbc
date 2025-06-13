83f4565857b1b51c02fe0de8a2ddf89d114ac1f6
Angelo <yapangeloterezo@gmail.com>
2025-06-13 12:56:11 -0700

    added sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,22 +1,45 @@
 import "./Sponsors.scss";
 import { Link } from "react-router-dom";
-import sponsor1 from "../../assets/images/sponsors/feast.jpg";
-import sponsor2 from "../../assets/images/sponsors/feast.jpg";
+import gold1 from "../../assets/images/sponsors/burnabysouthdental.jpg";
+import silver1 from "../../assets/images/sponsors/drfranciscavaldez.jpg";
 import sponsor3 from "../../assets/images/sponsors/feast.jpg";
 import sponsor4 from "../../assets/images/sponsors/feast.jpg";
 import sponsor5 from "../../assets/images/sponsors/feast.jpg";
 import sponsor6 from "../../assets/images/sponsors/feast.jpg";
 import Button from "../Button/Button";
-import SponsorTierList from "../SponsorTierList/SponsorTierList";
+// import SponsorTierList from "../SponsorTierList/SponsorTierList";
 
 const Sponsors = () => {
-  const sponsors = [{ image: sponsor1, link: "https://thefeastbc.com/" }];
+  const sponsorsgold = [{ image: gold1, link: "https://thefeastbc.com/" }];
+
+  const sponsorssilver = [
+    {
+      image: silver1,
+      link: "https://www.burnabysouthdental.com/",
+    },
+  ];
 
   return (
     <div className="sponsors">
       <h1 className="sponsors__title">EVENT SPONSORS</h1>
+      <h2 className="sponsors__tier">Gold</h2>
       <div className="sponsors__gallery">
-        {sponsors.map((sponsor, index) => (
+        {sponsorsgold.map((sponsor, index) => (
+          <div key={index} className="sponsors__gallery-imgcontainer">
+            <a href={sponsor.link} target="_blank" rel="noopener noreferrer">
+              <img
+                src={sponsor.image}
+                alt={`Sponsor ${index + 1}`}
+                className="sponsors__gallery-image"
+              />
+            </a>
+          </div>
+        ))}
+      </div>
+
+      <h2 className="sponsors__tier">Silver</h2>
+      <div className="sponsors__gallery">
+        {sponsorssilver.map((sponsor, index) => (
           <div key={index} className="sponsors__gallery-imgcontainer">
             <a href={sponsor.link} target="_blank" rel="noopener noreferrer">
               <img
@@ -31,7 +54,7 @@ const Sponsors = () => {
       <Link to="/sponsorpage">
         <Button text={"Be A Sponsor"} className={"button--primary"} />
       </Link>
-      <SponsorTierList />
+      {/* <SponsorTierList /> */}
     </div>
   );
 };
